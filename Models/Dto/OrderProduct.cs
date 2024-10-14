3c758918dbb1549ae085b33b1494b42857b61f14
Maximiliano Bertoli <maximiliano.bertoli@ff3d.com>
2024-10-14 22:59:30 +0200

    completato prj v1.0

--- Models/Dto/OrderProduct.cs
+++ Models/Dto/OrderProduct.cs
@@ -2,9 +2,8 @@
 {
     public class OrderProduct
     {
-        public int ProductId { get; set; }
         public int Quantity { get; set; }
 
-        public int OrderId { get; set; }
+        public int ProductId { get; set; }
     }
 }
